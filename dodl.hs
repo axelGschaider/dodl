e3bfd98f84aa70739f0810cbfeb73e71fd4c4ee6
Axel Gschaider <axel.gschaider@gmail.com>
2013-07-13 18:24:32 +0200

    keep together what belongs together

--- dodl.hs
+++ dodl.hs
@@ -11,36 +11,39 @@ main = do
   let rename = if isFolder then renameDirectory else renameFile
   rename file newFile
 
+
+
+
 offWithHisHead (x : []) = x
 offWithHisHead _        = error "usage: dodl file"
 
 delimiterChar = '/'
+
+newFileName x = do
+  fileExists <- doesFileOrFolderExist x
+  let newFilePath = transformPath x
+  newFileExists <- doesFileOrFolderExist newFilePath
+  return newFilePath
+
+transformPath f = gluePath $ stub ++ [newF]
+        where splited = Main.splitPath f
+	      stub = init splited
+	      newF = transformFileName $ last splited
+
+confirmDoesntExist f = do
+  exist <- doesFileOrFolderExist f
+  if exist then (error $ f ++ " already exists") else return f
+
+confirmExists :: String -> IO( String )
+confirmExists f = do
+  exist <- doesFileOrFolderExist f
+  if exist then (return f) else error $ f ++ " doesn't exist"
 
 -- there is some applicative way to do this.
 doesFileOrFolderExist f = do
   fileExist   <- doesFileExist f
   folderExist <- doesDirectoryExist f
   return (fileExist || folderExist)
-
-newFileName x = do
-  fileExists      <- doesFileOrFolderExist x
-	let newFilePath = transformPath x
-	newFileExists   <- doesFileOrFolderExist newFilePath
-	return newFilePath
-
-transformPath f = gluePath $ stub ++ [newF]
-        where splited = Main.splitPath f
-	            stub = init splited
-	            newF = transformFileName $ last splited
-
-confirmDoesntExist f = do
-        exist <- doesFileOrFolderExist f
-	     if exist then (error $ f ++ " already exists") else return f
-
-confirmExists :: String -> IO( String )
-confirmExists f = do
-        exist <- doesFileOrFolderExist f
-      	if exist then (return f) else error $ f ++ " doesn't exist"
 
 transformFileName ('.':f) = f
 transformFileName f = '.' : f
@@ -53,7 +56,6 @@ splitPath (x:xs) | (isDelimiter x) = "" : splitPath' xs
 splitPath xs = splitPath' xs
 
 splitPath' = wordsWhen isDelimiter
-
 isDelimiter = (== delimiterChar)
 
 wordsWhen     :: (Char -> Bool) -> String -> [String]
@@ -61,3 +63,4 @@ wordsWhen p s =  case dropWhile p s of
                       "" -> []
                       s' -> w : wordsWhen p s''
                            where (w, s'') = break p s'
+
